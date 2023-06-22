614abf814c014839354a007bb95f117dd9ab5d16
Nonthawt20 <Nonthawt20@gmail.com>
2023-06-22 21:25:28 +0700

    sending files failed

--- pages/register.tsx
+++ pages/register.tsx
@@ -1,53 +1,107 @@
+import { data } from "autoprefixer";
 import Head from "next/head";
-import { useEffect, useState } from 'react'
-import { useRouter } from 'next/router'
+import { useRouter } from 'next/router';
+import { useState } from 'react';
+
 
 export default function Register() {
 
-    const  router = useRouter()
+
+    const [formData, setFormData] = useState({
+        username: "",
+        password: "",
+        firstname: "",
+        lastname: "",
+        file: ""
+    });
+
+
+    const [formSuccess, setFormSuccess] = useState(false)
+    const [formSuccessMessage, setFormSuccessMessage] = useState("")
+
+    const handleInput = (e: any) => {
+        const fieldName = e.target.name;
+        const fieldValue = e.target.value;
+
+        setFormData((prevState) => ({
+            ...prevState,
+            [fieldName]: fieldValue
+        }));
+    }
+
+    const submitForm = (e: any) => {
+
+        e.preventDefault()
+
+        const formURL = e.target.action
+        const data = new FormData()
+
+        // data.append("profile_picture", e.target.files[0]);
+
+        Object.entries(formData).forEach(([key, value]) => {
+            data.append(key, value);
+        })
+
+
+        fetch(formURL, {
+            method: "POST",
+            body: data,
+            headers: {
+                'accept': 'application/json',
+            },
+        }).then((response) => {
+
+            return response.json()
+        })
+
+            .then((data) => {
+                console.log(data)
+                setFormData({
+                    username: "",
+                    password: "",
+                    firstname: "",
+                    lastname: "",
+                    file: ""
+                })
+                setFormSuccess(true)
+                setFormSuccessMessage(data.submission_text)
+            })
+            .catch(err => console.log(err))
+    }
 
 
     
 
 
-  
 
 
-
-
-
-
-        
     return (
-        <>
-            <Head>
-                <title> REGISTER </title>
+        <div>
+            <title> REGISTER </title>
+            <h1>REGISTER</h1>
+            {formSuccess ?
+                <div>{formSuccessMessage}</div>
+                :
+                <form action="http://localhost:3000/users/create" method="post" onSubmit={submitForm}>
 
-            </Head>
-            <div>
-
-
-                <Head>
-                    <body>
-                        {/* <meta name="page login pless login" content="Login page" /> */}
-                        <h1 > Register </h1>
-                    </body>
-
-                    <form action="/send-data-here" method="post">
-
-                        <label htmlFor="Username">Username:</label>
+                    <div>
+                        <label htmlFor="username">Username:</label>
                         <input
                             type="text"
-                            id="Username"
-                            name="Username"
+                            id="username"
+                            name="username"
                             required
                             autoFocus
                             minLength={4}
                             maxLength={12}
                             size={12}
-                            pattern="[A-Z|a-z|0-9|_]"
+                            onChange={handleInput}
+                        // pattern="[A-Z|a-z|0-9|_]"
 
                         />
+                    </div>
+
+                    <div>
                         <label htmlFor="password">Password:</label>
                         <input
                             type="password"
@@ -55,8 +109,11 @@ export default function Register() {
                             name="password"
                             required
                             minLength={6}
+                            onChange={handleInput}
                         />
+                    </div>
 
+                    <div>
                         <label htmlFor="Firstname">Firstname:</label>
                         <input
                             type="Firstname"
@@ -66,8 +123,11 @@ export default function Register() {
                             minLength={1}
                             maxLength={60}
                             size={60}
+                            onChange={handleInput}
                         />
+                    </div>
 
+                    <div>
                         <label htmlFor="Lastname">Lastname:</label>
                         <input
                             type="Lastname"
@@ -76,29 +136,28 @@ export default function Register() {
                             minLength={1}
                             maxLength={60}
                             size={60}
+                            onChange={handleInput}
                         />
+                    </div>
 
-                        <label htmlFor = "myfile"> Select a file:</label>
-                        <input type="file" id ="myfile" name="myfile"></input>
+                    <div>
+                        <label htmlFor="file"> Select a file:</label>
+                        <input
+                            type="file"
+                            id="file"
+                            name="file"
+                            onChange={handleInput}
 
-
-
-
-                        <button type="submit">Register</button>
-                    </form>
-
-
-
-                </Head>
-
+                        />
+                    </div>
 
 
+                    
 
-            </div>
+                    <button type="submit">Register</button>
+                </form>
 
-
-
-
-        </>
+            }
+        </div>
     )
 }
